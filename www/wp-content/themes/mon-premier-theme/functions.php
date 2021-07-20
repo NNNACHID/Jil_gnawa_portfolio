f97223b685e727f2e19d832a94c81f3980a3bb2b
NNACHID <nicolas.nachid@le-campus-numerique.fr>
2021-07-20 16:31:58 +0200

    end journey commit, add bulma and first article

--- www/wp-content/themes/mon-premier-theme/functions.php
+++ www/wp-content/themes/mon-premier-theme/functions.php
@@ -1,26 +1,35 @@
 <?php
 
-    /**
-     * Loading All CSS Stylesheets and Javascript Files.
-     */
-    function wordpress_scripts_loader()
-    {
-        $theme_version = wp_get_theme()->get('Version');
+/**
+ * Loading All CSS Stylesheets and Javascript Files.
+ */
+function wordpress_scripts_loader()
+{
+    $theme_version = wp_get_theme()->get('Version');
 
-        // 1. Styles.
-        wp_enqueue_style('style', get_template_directory_uri() . '/style.css', [], $theme_version, 'all');
-        wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.css', [], $theme_version, 'all'); // main.scss: Compiled Framework source + custom styles.
+    // 1. Styles.
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css', [], $theme_version, 'all');
+    wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.css', [], $theme_version, 'all'); // main.scss: Compiled Framework source + custom styles.
 
-        if (is_rtl()) {
-            wp_enqueue_style('rtl', get_template_directory_uri() . '/assets/css/rtl.css', [], $theme_version, 'all');
-        }
-
-        // 2. Scripts.
-        wp_enqueue_script('mainjs', get_template_directory_uri() . '/assets/js/main.bundle.js', [], $theme_version, true);
-
-        if (is_singular() && comments_open() && get_option('thread_comments')) {
-            wp_enqueue_script('comment-reply');
-        }
+    if (is_rtl()) {
+        wp_enqueue_style('rtl', get_template_directory_uri() . '/assets/css/rtl.css', [], $theme_version, 'all');
     }
 
-    add_action('wp_enqueue_scripts', 'wordpress_scripts_loader');
+    // 2. Scripts.
+    wp_enqueue_script('mainjs', get_template_directory_uri() . '/assets/js/main.bundle.js', [], $theme_version, true);
+
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
+}
+
+function mon_premier_theme_setup()
+{
+    add_theme_support('custom-logo');
+}
+
+add_action('wp_enqueue_scripts', 'wordpress_scripts_loader', 'mon_premier_theme_setup');
+
+
+
+
---
+++ www/wp-content/themes/mon-premier-theme/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JIL GNAWA</title>
+    <?php wp_head() ?>
+</head>
+
+<body>
+<div class="container is-fullhd">
+    <section class="hero is-info is-large">
+        <div class="hero-head">
+            <nav class="level">
+                <p class="level-item has-text-centered mt-4">
+                    <?php the_custom_logo(); ?>
+                </p>
+            </nav>
+        </div>
+        <div class="hero-body">
+            <div class="container has-text-centered">
+            </div>
+        </div>
+        <div class="hero-foot">
+            <nav class="tabs is-boxed is-fullwidth">
+                <div class="container">
+                    <ul>
+                        <li>
+                            <a>Elements</a>
+                        </li>
+                        <li>
+                            <a>Components</a>
+                        </li>
+                        <li>
+                            <a>Layout</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </section>
